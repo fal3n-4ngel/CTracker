cd2d7975a11a94c86193c4ee84a32ed84bb96e6d
fal3n-4ngel <adiadithyakrishnan@gmail.com>
2023-02-07 20:00:16 +0530

    homescreen works

--- src/CTracker/HomeScreen.java
+++ src/CTracker/HomeScreen.java
@@ -1,12 +1,27 @@
 package CTracker;
-import javax.swing.JLabel;
-import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JFrame;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionListener;
+import java.sql.ResultSet;
+import java.awt.event.ActionEvent;
+import CTracker.util.Connector;
+import CTracker.App.*;
+import CTracker.HomeScreen.*;
 
 public class HomeScreen extends JPanel{
     public JFrame parentFrame = null;
     HomeScreen(JFrame parentFrame){
-        JLabel l1=new JLabel("Home");
+        setLayout(new GridBagLayout());
+        JLabel l1=new JLabel("Home Screen");
         add(l1);
+
+        JPanel HomePanel=new JPanel();
+        HomePanel.setLayout(new BoxLayout(HomePanel,BoxLayout.Y_AXIS));
+        add(HomePanel);
     }
 }
